678d4112b66e8d7016a528ddf520477c114d76df
Samuel Araújo Silveira <samuel.silveira2001@gmail.com>
2024-08-31 13:38:18 -0300

    remocao das opçoes de de editar e eexlcuir medicos do perfil do medico. foi mantida a penas a opcao de visualizar com detalhes

--- saudeplus/urls.py
+++ saudeplus/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('medico/', include('medicos.urls')),
+    path('', include('medicos.urls')),
     path('admin/', admin.site.urls),
     path('usuario/', include('usuarios.urls')),
     path('conta/', include('django.contrib.auth.urls')),
