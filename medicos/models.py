6d3ad8b2b3dd645834a7e30d1b5dae582bfca30f
Samuel Araújo Silveira <samuel.silveira2001@gmail.com>
2024-08-31 01:43:52 -0300

    MODEL do imageField de medico alterado e problema do imageField resolvido. imagens sendo carregadas com sucesso

--- medicos/models.py
+++ medicos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from usuarios.models import Usuario
 
 class Medico(Usuario):
-    foto_perfil = models.ImageField(upload_to='images/medicos/', height_field=None, width_field=None, max_length=255, blank=True)
+    foto_perfil = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=255, blank=True)
     nome = models.CharField(max_length=100)
     crm= models.CharField(max_length=10)
     especialidade= models.CharField(max_length=50)
