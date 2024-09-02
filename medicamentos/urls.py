a8412c43d30ed1f5c195223fe919054ec2a2c69c
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2024-09-01 22:24:39 -0300

    Crud de medicamentos, faltando apenas delete

---
+++ medicamentos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.home, name="home-medicamento"),
+    path('add/', views.add, name="add-medicamento"),
+    path('detail/<int:id>/', views.detail, name="detail-medicamento"),
+    path('edit/<int:id>/', views.edit, name="edit-medicamento"), 
+]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import CustomLoginView
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('adm/', views.home, name='home-adm'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
