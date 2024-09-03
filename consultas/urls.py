4d252df61326e5ea4ff097db36388ab5bfdc9b5a
Samuel Araújo Silveira <samuel.silveira2001@gmail.com>
2024-09-03 02:05:48 -0300

    CRUD DE CONSULTA PRONTO

--- consultas/urls.py
+++ consultas/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('detail/<int:consulta_id>/', views.detail, name='detail-consulta'),
     path('edit/<int:consulta_id>/', views.edit, name='edit-consulta'),
     path('remove/<int:consulta_id>/', views.remove, name='remove-consulta'),
-    path('add/', views.add, name='add-consulta')
+    path('add/', views.add, name='add-consulta'),
+    path('detail/', views.detail, name='detail-consulta'),
+    path('editReceita/<int:receita_id>/', views.editar_receita, name='edit-consulta-receita')
+
 ]
