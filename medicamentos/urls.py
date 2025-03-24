5b7f19c03514602e17231feedb59c19dbd679652
Samuel Araújo Silveira <samuel.silveira2001@gmail.com>
2025-03-24 00:36:01 -0300

    feat: Atualiza rotas em medicamentos, medicos e saudeplus

--- medicamentos/urls.py
+++ medicamentos/urls.py
@@ -1,14 +1,11 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name="home-medicamento"),
-    path('add/', views.add, name="add-medicamento"),
-    path('detail/<int:id>/', views.detail, name="detail-medicamento"),
-    path('edit/<int:id>/', views.edit, name="edit-medicamento"), 
-    path('remove/<int:id>/', views.remove, name="remove-medicamento"), 
-    path('api/', views.MedicamentosList.as_view()),
-    path('api/<int:id>/', views.MedicamentoViewDetail.as_view()),
-]
+    path('read/', views.home, name="read-medicamento"),
+    path('api/', views.MedicamentosList.as_view(), name='api-medicamentos'),
+    path('api/<int:id>/', views.MedicamentoViewDetail.as_view(), name='api-medicamento-detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
