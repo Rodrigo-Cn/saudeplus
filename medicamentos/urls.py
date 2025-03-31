474ac426b576e8b698dbf6ed1c66d4d90fc960e7
Samuel Araújo SIlveira <samuel.silveira2001@gmail.com>
2025-03-31 10:10:06 -0300

    Feat: Conserta URLs

--- medicamentos/urls.py
+++ medicamentos/urls.py
@@ -4,8 +4,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    # Rotas principais
     path('', views.home, name="home-medicamento"),
     path('read/', views.home, name="read-medicamento"),
+    
+    # Operações de CRUD
+    path('add/', views.add, name="add-medicamento"),
+    path('detail/<int:id>/', views.detail, name="detail-medicamento"),
+    path('edit/<int:id>/', views.edit, name="edit-medicamento"),
+    path('remove/<int:id>/', views.remove, name="remove-medicamento"),
+    
+    # API
     path('api/', views.MedicamentosList.as_view(), name='api-medicamentos'),
     path('api/<int:id>/', views.MedicamentoViewDetail.as_view(), name='api-medicamento-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- medicos/urls.py
+++ medicos/urls.py
@@ -4,8 +4,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    # Rotas tradicionais (HTML)
     path('', views.home, name='home'),
     path('read/', views.home, name='read-medico'),
+    path('detail/<int:medico_id>/', views.detail, name='detail-medico'),
+    path('detail2/<int:medico_id>/', views.detail2, name='detail2-medico'),
+    path('edit/<int:medico_id>/', views.edit, name='edit-medico'),
+    path('perfil/', views.perfil, name='perfil-medico'),
+    path('editperfil/', views.editperfil, name='edit-perfil'),
+    path('remove/<int:medico_id>/', views.remove, name='remove-medico'),
+    path('add/', views.add, name='add-medico'),
+    # Endpoints da API
     path('api/', views.MedicosList.as_view(), name='api-medicos'),
     path('api/<int:id>/', views.MedicoViewDetail.as_view(), name='api-medico-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
